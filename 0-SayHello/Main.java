437af8bb60b18a1b22ceeb2842d7459177d64346
Tenzenmaster <160365410+Tenzenmaster@users.noreply.github.com>
2024-09-02 16:19:04 -0400

    assignment 0

---
+++ 0-SayHello/Main.java
@@ -0,0 +1,14 @@
+/*
+Tenzen Nicholas
+tnicho16@u.rochester.edu
+9/2/2024
+CSC-171 Assignment 0: Say Hello
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Tenzen Nicholas");
+        System.out.println("tnicho16@u.rochester.edu");
+        System.out.println("I've been trying out Neovim but setting up the Java LSP is so hard!!!");
+    }
+}
